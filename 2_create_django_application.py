10237cc5d2a0676c2ef33ea2abbc2176e66adbb9
bbong718 <bbong718@gmail.com>
2017-01-08 00:16:32 -0500

    Corrected django.config settings

--- 2_create_django_application.py
+++ 2_create_django_application.py
@@ -2,7 +2,6 @@ import os, sys
 from subprocess import call
 
 PROJECT_INDENTATION = '.' * 3
-PROJECT_MIN_LEN = 4
 PROJECT_EB_JANGO = ['django-admin', 'startproject', 'ebdjango']
 PROJECT_START = ['python', 'manage.py', 'runserver']
 PROJECT_REQUIREMENTS = 'pip freeze > requirements.txt'
@@ -11,11 +10,12 @@ PROJECT_EB_DJANGO_CONF = 'django.config'
 PROJECT_EB_DJANGO_CONF_DATA = """
 option_settings:
       aws:elasticbeanstalk:container:python:
-          WSGIPath: %s/wsgi.py
+          WSGIPath: %s/%s/wsgi.py
 """
 
+
 if not hasattr(sys, 'real_prefix'):
-    print "[!] Please run in me in virtualenv on your project!"
+    print "[!] Please run me in virtualenv on your project!"
     exit()
 
 def create_new_project():
@@ -46,13 +46,21 @@ def create_ebextensions():
     print PROJECT_INDENTATION + '[+] Creating ' + PROJECT_EB_EXTENSION
     try:
         os.mkdir(PROJECT_EB_EXTENSION)
-    except:
+    except BaseException as e:
         print PROJECT_INDENTATION + '[!] Failed to create EB Extensions directory'
 
     eb_ext_fh = open(PROJECT_EB_EXTENSION + '/' + PROJECT_EB_DJANGO_CONF, "w")
     eb_ext_fh.truncate()
-    eb_ext_fh.write(PROJECT_EB_DJANGO_CONF_DATA % PROJECT_EB_JANGO[2])
+    eb_ext_fh.write(PROJECT_EB_DJANGO_CONF_DATA % (PROJECT_EB_JANGO[2], PROJECT_EB_JANGO[2]))
     eb_ext_fh.close()
+
+def get_python_version():
+    print PROJECT_INDENTATION + '[?] Figuring out Python version...',
+    for py in os.listdir('lib')
+        if 'python' in py:
+            break
+    py
+    return py
 
 os.chdir(os.getcwd())
 create_new_project()
@@ -61,3 +69,8 @@ create_ebextensions()
 
 os.chdir(PROJECT_EB_JANGO[2])
 run_manage()
+
+# Move up one path so we can get the python version
+os.chdir('..')
+my_py_ = get_python_version()
+
